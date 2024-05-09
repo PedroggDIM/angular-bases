016839dcad7c0d6d06375044dae0533c47218714
Pedro Gil <pedrogg76@yahoo.es>
2024-05-09 14:47:35 +0200

    74. Servicio Privado

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -9,6 +9,21 @@ import { DbzService } from '../services/dbz.services';
 
 export class MainPageComponent {
 
-  constructor( public dbzService: DbzService ){}
+  constructor( private dbzService: DbzService ){}
 
+  get characters(): Character[] {  // get de characters que devuelve un arreglo de tipo character[]
+    //Aquí se está utilizando el operador spread (...) para crear una copia del
+    // array characters que está almacenado en el servicio DbzService.
+    // Esto se hace para evitar la modificación directa del array original. Devuelve esta copia del array.
+    return [...this.dbzService.characters];
+  }
+
+  onDeleteCharacter( id: string ) : void {
+    this.dbzService.deleteCharacterById( id );
+  }
+
+  onNewCharacter( Character: Character ): void {
+    this.dbzService.addCharacter( Character );
+
+  }
 }
